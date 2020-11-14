e09ae61a88c06adb94c51389741e7934dc7c9d0e
Fouad Aoude <fouad@Fouads-MacBook-Pro.local>
2020-11-14 17:03:59 -0500

    Finished mixedMessages. Too easy

--- mixedMessages/script.js
+++ mixedMessages/script.js
@@ -1,3 +1,20 @@
 // script.js
 
 const wordList = require('an-array-of-english-words');
+const makeSense = ['I', 'Hate', 'Love', 'When', 'Let', 'Me', 'Eat', 'Together', 'Alone', 'Forever', 'With', 'Vacation', 'Jail', 'Go', 'With', 'Music', 'Cool', 'Hands', 'Feet', 'Smell'];
+
+const getRandWords = () => {
+    let words = [];
+
+    for (let i=0;i<3;i++) {
+        let numWordList = Math.floor(Math.random() * wordList.length);
+        let numMakeSense = Math.floor(Math.random() * makeSense.length);
+        words.push(makeSense[numMakeSense]);
+        words.push(wordList[numWordList]);
+    }
+
+    return words;
+};
+
+words = getRandWords();
+console.log(words.join(' '));
